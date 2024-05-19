96cbde4ff9860e5602f1cfc5b75ed8d0cf0ae290
madhavsGIT <kmadhav455@gamil.com>
2024-05-20 05:16:19 +0530

    3rd

--- src/main/java/com/driver/StudentController.java
+++ src/main/java/com/driver/StudentController.java
@@ -19,8 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("students")
 public class StudentController {
 
-    @Autowired
-    private StudentService studentService;
+
+    StudentService studentService = new StudentService();
 
     @PostMapping("/add-student")
     public ResponseEntity<String> addStudent(@RequestBody Student student){
--- src/main/java/com/driver/StudentService.java
+++ src/main/java/com/driver/StudentService.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class StudentService {
 
-    @Autowired
-    StudentRepository studentRepository;
+
+    StudentRepository studentRepository = new StudentRepository();
 
     public void addStudent(Student student){
         studentRepository.saveStudent(student);
