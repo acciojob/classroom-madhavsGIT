21b0627d070543fbd16cceabb0ccad43519c4664
madhavsGIT <kmadhav455@gamil.com>
2024-05-20 04:42:12 +0530

    first commit

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -19,40 +19,77 @@ public class StudentRepository {
 
     public void saveStudent(Student student){
         // your code goes here
+        String key = student.getName();
+        studentMap.put(key,student);
+
     }
 
     public void saveTeacher(Teacher teacher){
         // your code goes here
+        String key = teacher.getName();
+        teacherMap.put(key,teacher);
     }
 
     public void saveStudentTeacherPair(String student, String teacher){
         if(studentMap.containsKey(student) && teacherMap.containsKey(teacher)){
             // your code goes here
+            List<String> studentList = teacherStudentMapping.get(teacher);
+            studentList.add(student);
+            teacherStudentMapping.put(teacher,studentList);
         }
+
     }
 
     public Student findStudent(String student){
         // your code goes here
+        if(studentMap.containsKey(student)) {
+            return studentMap.get(student);
+        }
+        return null;
     }
 
     public Teacher findTeacher(String teacher){
         // your code goes here
+        if(teacherMap.containsKey(teacher)){
+            return teacherMap.get(teacher);
+        }
+        return null;
     }
 
     public List<String> findStudentsFromTeacher(String teacher){
         // your code goes here
         // find student list corresponding to a teacher
+
+        if(teacherStudentMapping.containsKey(teacher)){
+            return teacherStudentMapping.get(teacher);
+        }
+        return null;
     }
 
     public List<String> findAllStudents(){
         // your code goes here
+        List<String> allStudentsList = new ArrayList<>();
+
+        for(Student student : studentMap.values()){
+            allStudentsList.add(student.getName());
+        }
+        return allStudentsList;
     }
 
     public void deleteTeacher(String teacher){
         // your code goes here
+        if(teacherMap.containsKey(teacher)){
+            teacherMap.remove(teacher);
+        }
+        if(teacherStudentMapping.containsKey(teacher)){
+             teacherStudentMapping.remove(teacher);
+        }
+
     }
 
     public void deleteAllTeachers(){
         // your code goes here
+        teacherMap.clear();
+        teacherStudentMapping.clear();
     }
 }
